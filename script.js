fc45b7964a2f56562efe5acbae3d8fc0e2860eee
Martin Lozano <martin.lozano995@gmail.com>
2024-09-20 21:55:23 -0300

    Correccion de que se ve la barra de navegacion al comenzar

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ function closeCard() {
     const card = document.getElementById("persona5-card");
     const music = document.getElementById("backgroundMusic"); // Referencia al audio
     const chapters = document.querySelectorAll(".chapter"); // Selecciona todas las secciones
-
+    const nav = document.querySelector('nav');
+    nav.style.opacity = '1';
     card.style.opacity = '0'; // Desvanecemos la tarjeta
     setTimeout(() => {
         card.style.display = 'none'; // Ocultamos la tarjeta
-        music.play().catch(function(error) {
+        music.play().catch(function (error) {
             console.log('Error al intentar reproducir la música:', error);
         });
-        
+nav.classList.add("active")
         // Hacemos visible el contenido de los capítulos
         chapters.forEach(chapter => {
             chapter.classList.add("active");
